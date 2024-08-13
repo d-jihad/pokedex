bb67778c4926e46dd1957a52f6996e2ab43644e7
Djihad Boukara <djihad.boukara@proton.me>
2024-08-13 16:44:41 +0200

    added cache to improve performance

--- internals/pokeapi/client.go
+++ internals/pokeapi/client.go
@@ -1,16 +1,19 @@
 package pokeapi
 
 import (
+	"github.com/d-jihad/pokedex/internals/pokecache"
 	"net/http"
 	"time"
 )
 
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout time.Duration, interval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(interval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
